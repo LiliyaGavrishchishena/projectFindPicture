d3b11e06a33c59fc303fc3152fc043c12ed34d48
Liliya Gavrishchishena <Liliya.Gavrishchishena@gmail.com>
2018-10-25 23:39:09 +0300

    merged files from 3 branches

--- src/js/services/api_pixabay.js
+++ src/js/services/api_pixabay.js
@@ -2,10 +2,23 @@
 // before using axios you have to install them, "npm i axios"
 import axios from 'axios';
 
-const BASE_URL = "https://pixabay.com/api/";
-const API_KEY = "10482228-408f6b22fcb86c00bbed30e3f";
+const BASE_URL = 'https://pixabay.com/api/';
+const API_KEY = '10482228-408f6b22fcb86c00bbed30e3f';
 const RETURN_QUANTITY = '12';
 
-export const getImages = (request,page) =>{
-    return fetch(`${BASE_URL}?key=${API_KEY}&q=${request}&per_page=${RETURN_QUANTITY}&page=${page}`).then((request)=>{if (request.ok){return request.json()}}).then((data)=>{return data.hits}).catch((err)=>{console.log(`Error while fetching, with status-code ${err.status}`)});
-}
+export const getImages = (request, page) => {
+  return fetch(
+    `${BASE_URL}?key=${API_KEY}&q=${request}&per_page=${RETURN_QUANTITY}&page=${page}`
+  )
+    .then(request => {
+      if (request.ok) {
+        return request.json();
+      }
+    })
+    .then(data => {
+      return data.hits;
+    })
+    .catch(err => {
+      console.log(`Error while fetching, with status-code ${err.status}`);
+    });
+};
