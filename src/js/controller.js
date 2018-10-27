358ca2a44bbb7f4c57fcc96809c4d55378323979
Oksana <kursorksu777@gmail.com>
2018-10-28 00:04:42 +0300

    favourites

--- src/js/controller.js
+++ src/js/controller.js
@@ -3,69 +3,72 @@ export default class Controller {
     this._view = view;
     this._model = model;
 
-    this._view.refs.form.addEventListener(
+    this._view.form.addEventListener(
       'submit',
       this.handleFormSubmit.bind(this));
 
-    this._view.refs.showMoreBtn.addEventListener(
+    this._view.showMoreBtn.addEventListener(
       'click',
       this.handleShowMore.bind(this));
 
-    this._view.refs.pictList.addEventListener(
+    this._view.pictList.addEventListener(
       'click',
       this.popUpOpen.bind(this));
 
-    this._view.refs.close.addEventListener(
+    this._view.close.addEventListener(
       'click',
       this.popUpClose.bind(this));
 
-    this._view.refs.next.addEventListener(
+    this._view.next.addEventListener(
       'click',
       this.popUpNext.bind(this));
 
-    this._view.refs.prev.addEventListener(
+    this._view.prev.addEventListener(
       'click',
       this.popUpPrev.bind(this));
 
-    this._view.refs.select.addEventListener(
+    this._view.select.addEventListener(
       'click',
       this.popUpSelect.bind(this));
 
-    this._view.refs.headerLogo.addEventListener(
+    this._view.headerLogo.addEventListener(
       'click',
       this.handleHeaderLogo.bind(this));
 
-    this._view.refs.btnFavourite.addEventListener(
+    this._view.btnFavourite.addEventListener(
       'click',
       this.handleFavouriteList.bind(this));
 
+    this._view.delete.addEventListener(
+        'click',
+        this.favoriteItemDelite.bind(this));
   }
 
   handleFormSubmit(e) {
     e.preventDefault();
-    const input = this._view.refs.input;
+    const input = this._view.input;
 
     this._model.getRequest(input.value).then(createdItems => {
       if (createdItems === undefined) return;
       this._view.init(createdItems);
       if (createdItems.length !== 0) {
-        this._view.refs.showMoreBtn.classList.add('active');
+        this._view.showMoreBtn.classList.add('active');
       } else {
-        this._view.refs.showMoreBtn.classList.remove('active');
+        this._view.showMoreBtn.classList.remove('active');
       }
     });
   }
 
   handleShowMore(e) {
     e.preventDefault();
-    const val = this._view.refs.input.value;
+    const val = this._view.input.value;
     this._model.getRequest(val).then(createdItems => {
       if (createdItems === undefined) return;
       this._view.addPicture(createdItems);
       if (createdItems.length !== 0) {
-        this._view.refs.showMoreBtn.classList.add('active');
+        this._view.showMoreBtn.classList.add('active');
       } else {
-        this._view.refs.showMoreBtn.classList.remove('active');
+        this._view.showMoreBtn.classList.remove('active');
       }
     });
   }
@@ -74,109 +77,145 @@ export default class Controller {
     const target = e.target;
     const targetImg = target.querySelector('img');
     if (target.nodeName !== 'LI') return;
-    this._view.refs.page.classList.add('modale-open');
-    this._view.refs.modaleImage.setAttribute('src', targetImg.dataset.fullview);
-    this._view.refs.modaleImage.setAttribute(
+    this._view.page.classList.add('modale-open');
+    this._view.modaleImage.setAttribute('src', targetImg.dataset.fullview);
+    this._view.modaleImage.setAttribute(
       'alt',
       targetImg.getAttribute('alt')
     );
-    this._view.refs.modaleImage.setAttribute(
-      'id',
-      targetImg.parentNode.dataset.idItem
-    );
-
-    const activeImgUrl = this._view.refs.modaleImage.getAttribute('src');
-    const itemList = this._view.refs.pictList.querySelectorAll('img');
+    const activeImgUrl = this._view.modaleImage.getAttribute('src');
+    const itemList = this._view.pictList.querySelectorAll('img');
     Array.from(itemList).map(img => {
       if (img.dataset.fullview === activeImgUrl) {
         const currentNumber = Array.from(itemList).indexOf(img);
         if (currentNumber + 1 === itemList.length) {
-          this._view.refs.next.setAttribute('disabled', 'disabled');
+          this._view.next.setAttribute('disabled', 'disabled');
         }
         if (currentNumber === 0) {
-          this._view.refs.prev.setAttribute('disabled', 'disabled');
+          this._view.prev.setAttribute('disabled', 'disabled');
         }
       }
     });
   }
 
   popUpClose() {
-    this._view.refs.page.classList.remove('modale-open');
-    this._view.refs.prev.removeAttribute('disabled');
-    this._view.refs.next.removeAttribute('disabled');
+    this._view.page.classList.remove('modale-open');
+    this._view.prev.removeAttribute('disabled');
+    this._view.next.removeAttribute('disabled');
   }
 
   popUpNext() {
-    //Это точка отсчета == ссылка активного слайда
-    const activeImgUrl = this._view.refs.modaleImage.getAttribute('src');
-    //Это массив всех изображений
-    const itemList = this._view.refs.pictList.querySelectorAll('img');
+    const activeImgUrl = this._view.modaleImage.getAttribute('src');
+    const itemList = this._view.pictList.querySelectorAll('img');
+
     Array.from(itemList).map(img => {
       if (img.dataset.fullview === activeImgUrl) {
         const currentNumber = Array.from(itemList).indexOf(img);
         const next = Array.from(itemList)[currentNumber + 1];
         if (currentNumber + 1 < itemList.length) {
-          console.log('Номер следующего слайда меньше длины');
-          this._view.refs.next.removeAttribute('disabled');
-          this._view.refs.prev.removeAttribute('disabled');
-          this._view.refs.modaleImage.setAttribute(
+          this._view.next.removeAttribute('disabled');
+          this._view.prev.removeAttribute('disabled');
+          this._view.modaleImage.setAttribute(
             'src',
             next.dataset.fullview
           );
         }
         if (currentNumber + 1 === itemList.length - 1) {
-          console.log('Номер следующего слайда Равен длине');
-          this._view.refs.next.setAttribute('disabled', 'disabled');
+          this._view.next.setAttribute('disabled', 'disabled');
         }
       }
     });
+
   }
 
   popUpPrev() {
-    const activeImgUrl = this._view.refs.modaleImage.getAttribute('src');
-    const itemList = this._view.refs.pictList.querySelectorAll('img');
+    const activeImgUrl = this._view.modaleImage.getAttribute('src');
+    const itemList = this._view.pictList.querySelectorAll('img');
     Array.from(itemList).map(img => {
       if (img.dataset.fullview === activeImgUrl) {
         const currentNumber = Array.from(itemList).indexOf(img);
         const next = Array.from(itemList)[currentNumber - 1];
-        this._view.refs.modaleImage.setAttribute('src', next.dataset.fullview);
+        this._view.modaleImage.setAttribute('src', next.dataset.fullview);
         if (currentNumber - 1 > 0) {
-          //console.log('Номер предыдущего слайда:',currentNumber - 1, '>0')
-          this._view.refs.next.removeAttribute('disabled');
-          this._view.refs.modaleImage.setAttribute(
+          this._view.next.removeAttribute('disabled');
+          this._view.modaleImage.setAttribute(
             'src',
             next.dataset.fullview
           );
         } else if (currentNumber - 1 === 0) {
-          //console.log('Номер предыдущего слайда:',currentNumber - 1, '=0')
-          this._view.refs.prev.setAttribute('disabled', 'disabled');
+          this._view.prev.setAttribute('disabled', 'disabled');
         }
       }
     });
   }
 
-  popUpSelect(e) {
+  getId(){
+      const itemList = this._view.pictList.querySelectorAll('li');
+      const carrentSrc = this._view.modaleImage.getAttribute('src');
+      let currentId;
+      Array.from(itemList).map(item => {
+          if(carrentSrc === item.querySelector('img').dataset.fullview){
+              currentId = item.dataset.idItem
+          }
+      });
+      return currentId;
+  }
 
+  popUpSelect(e) {
+      const currentId = this.getId();
+      //вызывает функцию, Добавить в избранное на текущем iD
+      const itemObj = this._model.addToFavorite(currentId);
+      this._view.addFavoritesPicture(itemObj);
+  }
+//Єто не готово
+  favoriteItemDelite(e) {
+    console.log(target)
+      const currentId = target.id;
+      this._model.removeFromFavorite(currentId)
   }
 
   handleHeaderLogo(e) {
     e.preventDefault();
     this._view.init([]);
-    this._view.refs.input.value = '';
-    this._view.refs.showMoreBtn.classList.remove('active');
+    this._view.input.value = '';
+    this._view.showMoreBtn.classList.remove('active');
   }
 
   handleFavouriteList(e) {
     e.preventDefault();
-    console.log(this._view.refs.btnFavourite.classList.contains('js-home'));
-    if (this._view.refs.btnFavourite.classList.contains('js-home')) {
-      this._view.refs.btnFavourite.classList.remove('js-home');
-      this._view.refs.page.classList.remove('favorites--active');
-      this._view.refs.btnFavourite.textContent = 'Избранное';
+    //console.log(this._view.btnFavourite.classList.contains('js-home'));
+    if (this._view.btnFavourite.classList.contains('js-home')) {
+      this._view.btnFavourite.classList.remove('js-home');
+      this._view.page.classList.remove('favorites--active');
+      this._view.btnFavourite.textContent = 'Избранное';
       return;
     }
-    this._view.refs.page.classList.add('favorites--active');
-    this._view.refs.btnFavourite.textContent = 'На главную';
-    this._view.refs.btnFavourite.classList.add('js-home');
+    this._view.page.classList.add('favorites--active');
+    this._view.btnFavourite.textContent = 'На главную';
+    this._view.btnFavourite.classList.add('js-home');
   }
 }
+
+// {largeImageURL: "https://pixabay.com/get/ea32b70720f1083ed1584d05fb1d4796e77fe0d71bb90c4090f4c17fa0efb4b9d0_1280.jpg", webformatHeight: 426,
+//     webformatWidth: 640,
+//     likes: 40, imageWidth: 5905,
+//      …}comments: 40
+// downloads: 1094
+// favorites: 9
+// id: 3768859
+// imageHeight:3938
+// imageSize: 5472525
+// imageWidth: 5905
+// largeImageURL: "https://pixabay.com/get/ea32b70720f1083ed1584d05fb1d4796e77fe0d71bb90c4090f4c17fa0efb4b9d0_1280.jpg"
+// likes: 40
+// pageURL: "https://pixabay.com/en/rose-romantic-love-rose-bloom-3768859/"
+// previewHeight: 99
+// previewURL: "https://cdn.pixabay.com/photo/2018/10/23/20/12/rose-3768859_150.jpg"
+// previewWidth: 150tags: "rose, romantic, love"type: "photo"user: "KleineKiwi"
+// userImageURL: "https://cdn.pixabay.com/user/2018/10/07/19-51-01-992_250x250.jpeg"
+// user_id: 9883074
+// views: 1336
+// webformatHeight: 426
+// webformatURL: "https://pixabay.com/get/ea32b70720f1083ed1584d05fb1d4796e77fe0d71bb90c4090f4c17fa0efb4b9d0_640.jpg"
+// webformatWidth: 640
+// __proto__: Object
