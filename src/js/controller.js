2ce0bf0b1912479a4733b03f569b97c3fcb3a1ab
Liliya Gavrishchishena <Liliya.Gavrishchishena@gmail.com>
2018-10-27 20:16:42 +0300

    fixed view and button showmore

--- src/js/controller.js
+++ src/js/controller.js
@@ -4,28 +4,41 @@ export default class Controller {
     this._model = model;
 
     this._view.refs.form.addEventListener(
-      "submit",
-      this.handleFormSubmit.bind(this)
-    );
+      'submit',
+      this.handleFormSubmit.bind(this));
 
     this._view.refs.showMoreBtn.addEventListener(
-      "click",
-      this.handleShowMore.bind(this)
-    );
+      'click',
+      this.handleShowMore.bind(this));
 
     this._view.refs.pictList.addEventListener(
-      "click",
-      this.popUpOpen.bind(this)
-    );
+      'click',
+      this.popUpOpen.bind(this));
 
-    this._view.refs.close.addEventListener("click", this.popUpClose.bind(this));
-    this._view.refs.next.addEventListener("click", this.popUpNext.bind(this));
-    this._view.refs.prev.addEventListener("click", this.popUpPrev.bind(this));
+    this._view.refs.close.addEventListener(
+      'click',
+      this.popUpClose.bind(this));
+
+    this._view.refs.next.addEventListener(
+      'click',
+      this.popUpNext.bind(this));
+
+    this._view.refs.prev.addEventListener(
+      'click',
+      this.popUpPrev.bind(this));
+
+    this._view.refs.select.addEventListener(
+      'click',
+      this.popUpSelect.bind(this));
 
     this._view.refs.headerLogo.addEventListener(
-      "click",
-      this.handleHeaderLogo.bind(this)
-    );
+      'click',
+      this.handleHeaderLogo.bind(this));
+
+    this._view.refs.btnFavourite.addEventListener(
+      'click',
+      this.handleFavouriteList.bind(this));
+
   }
 
   handleFormSubmit(e) {
@@ -36,13 +49,11 @@ export default class Controller {
       if (createdItems === undefined) return;
       this._view.init(createdItems);
       if (createdItems.length !== 0) {
-        this._view.refs.showMoreBtn.classList.add("active");
+        this._view.refs.showMoreBtn.classList.add('active');
       } else {
-        this._view.refs.showMoreBtn.classList.remove("active");
+        this._view.refs.showMoreBtn.classList.remove('active');
       }
     });
-
-    //input.value = '';
   }
 
   handleShowMore(e) {
@@ -52,101 +63,120 @@ export default class Controller {
       if (createdItems === undefined) return;
       this._view.addPicture(createdItems);
       if (createdItems.length !== 0) {
-        this._view.refs.showMoreBtn.classList.add("active");
+        this._view.refs.showMoreBtn.classList.add('active');
       } else {
-        this._view.refs.showMoreBtn.classList.remove("active");
+        this._view.refs.showMoreBtn.classList.remove('active');
       }
     });
   }
 
   popUpOpen(e) {
     const target = e.target;
-    const targetImg = target.querySelector("img");
-    if (target.nodeName !== "LI") return;
-    this._view.refs.page.classList.add("modale-open");
-    this._view.refs.modaleImage.setAttribute("src", targetImg.dataset.fullview);
+    const targetImg = target.querySelector('img');
+    if (target.nodeName !== 'LI') return;
+    this._view.refs.page.classList.add('modale-open');
+    this._view.refs.modaleImage.setAttribute('src', targetImg.dataset.fullview);
     this._view.refs.modaleImage.setAttribute(
-      "alt",
-      targetImg.getAttribute("alt")
+      'alt',
+      targetImg.getAttribute('alt')
     );
     this._view.refs.modaleImage.setAttribute(
-      "id",
-      targetImg.getAttribute("id")
+      'id',
+      targetImg.parentNode.dataset.idItem
     );
 
-    const activeImgUrl = this._view.refs.modaleImage.getAttribute("src");
-    const itemList = this._view.refs.pictList.querySelectorAll("img");
+    const activeImgUrl = this._view.refs.modaleImage.getAttribute('src');
+    const itemList = this._view.refs.pictList.querySelectorAll('img');
     Array.from(itemList).map(img => {
       if (img.dataset.fullview === activeImgUrl) {
         const currentNumber = Array.from(itemList).indexOf(img);
         if (currentNumber + 1 === itemList.length) {
-          this._view.refs.next.setAttribute("disabled", "disabled");
+          this._view.refs.next.setAttribute('disabled', 'disabled');
         }
         if (currentNumber === 0) {
-          this._view.refs.prev.setAttribute("disabled", "disabled");
+          this._view.refs.prev.setAttribute('disabled', 'disabled');
         }
       }
     });
   }
 
   popUpClose() {
-    this._view.refs.page.classList.remove("modale-open");
-    this._view.refs.prev.removeAttribute("disabled");
-    this._view.refs.next.removeAttribute("disabled");
+    this._view.refs.page.classList.remove('modale-open');
+    this._view.refs.prev.removeAttribute('disabled');
+    this._view.refs.next.removeAttribute('disabled');
   }
 
   popUpNext() {
     //Это точка отсчета == ссылка активного слайда
-    const activeImgUrl = this._view.refs.modaleImage.getAttribute("src");
+    const activeImgUrl = this._view.refs.modaleImage.getAttribute('src');
     //Это массив всех изображений
-    const itemList = this._view.refs.pictList.querySelectorAll("img");
+    const itemList = this._view.refs.pictList.querySelectorAll('img');
     Array.from(itemList).map(img => {
       if (img.dataset.fullview === activeImgUrl) {
         const currentNumber = Array.from(itemList).indexOf(img);
         const next = Array.from(itemList)[currentNumber + 1];
         if (currentNumber + 1 < itemList.length) {
-          console.log("Номер следующего слайда меньше длины");
-          this._view.refs.next.removeAttribute("disabled");
-          this._view.refs.prev.removeAttribute("disabled");
+          console.log('Номер следующего слайда меньше длины');
+          this._view.refs.next.removeAttribute('disabled');
+          this._view.refs.prev.removeAttribute('disabled');
           this._view.refs.modaleImage.setAttribute(
-            "src",
+            'src',
             next.dataset.fullview
           );
         }
         if (currentNumber + 1 === itemList.length - 1) {
-          console.log("Номер следующего слайда Равен длине");
-          this._view.refs.next.setAttribute("disabled", "disabled");
+          console.log('Номер следующего слайда Равен длине');
+          this._view.refs.next.setAttribute('disabled', 'disabled');
         }
       }
     });
   }
 
   popUpPrev() {
-    const activeImgUrl = this._view.refs.modaleImage.getAttribute("src");
-    const itemList = this._view.refs.pictList.querySelectorAll("img");
+    const activeImgUrl = this._view.refs.modaleImage.getAttribute('src');
+    const itemList = this._view.refs.pictList.querySelectorAll('img');
     Array.from(itemList).map(img => {
       if (img.dataset.fullview === activeImgUrl) {
         const currentNumber = Array.from(itemList).indexOf(img);
         const next = Array.from(itemList)[currentNumber - 1];
-        this._view.refs.modaleImage.setAttribute("src", next.dataset.fullview);
+        this._view.refs.modaleImage.setAttribute('src', next.dataset.fullview);
         if (currentNumber - 1 > 0) {
           //console.log('Номер предыдущего слайда:',currentNumber - 1, '>0')
-          this._view.refs.next.removeAttribute("disabled");
+          this._view.refs.next.removeAttribute('disabled');
           this._view.refs.modaleImage.setAttribute(
-            "src",
+            'src',
             next.dataset.fullview
           );
         } else if (currentNumber - 1 === 0) {
           //console.log('Номер предыдущего слайда:',currentNumber - 1, '=0')
-          this._view.refs.prev.setAttribute("disabled", "disabled");
+          this._view.refs.prev.setAttribute('disabled', 'disabled');
         }
       }
     });
   }
 
+  popUpSelect(e) {
+
+  }
+
   handleHeaderLogo(e) {
     e.preventDefault();
     this._view.init([]);
-    this._view.refs.input.value = "";
+    this._view.refs.input.value = '';
+    this._view.refs.showMoreBtn.classList.remove('active');
+  }
+
+  handleFavouriteList(e) {
+    e.preventDefault();
+    console.log(this._view.refs.btnFavourite.classList.contains('js-home'));
+    if (this._view.refs.btnFavourite.classList.contains('js-home')) {
+      this._view.refs.btnFavourite.classList.remove('js-home');
+      this._view.refs.page.classList.remove('favorites--active');
+      this._view.refs.btnFavourite.textContent = 'Избранное';
+      return;
+    }
+    this._view.refs.page.classList.add('favorites--active');
+    this._view.refs.btnFavourite.textContent = 'На главную';
+    this._view.refs.btnFavourite.classList.add('js-home');
   }
 }
--- src/js/view.js
+++ src/js/view.js
@@ -9,7 +9,6 @@ export default class View {
     this.refs.pictList = document.querySelector('.picture__list');
     this.refs.showMoreBtn = document.querySelector('.js-more');
     this.refs.page = document.querySelector('body');
-    //=============================
     this.refs.delete = document.querySelector('.js-delete');
     this.refs.listFavorites = document.querySelector('.picture__list.favorites__list');
     this.refs.btnFavourite = document.querySelector('.header__favorites');
@@ -19,7 +18,7 @@ export default class View {
     this.refs.next = document.querySelector('.js-next');
     this.refs.select = document.querySelector('.js-select');
     this.refs.close = document.querySelector('.js-close');
-    this.refs.headerLogo = document.querySelector(".header__logo");
+    this.refs.headerLogo = document.querySelector('.header__logo');
   }
 
   init(items) {
@@ -42,96 +41,3 @@ export default class View {
   }
 
 }
-// const refs = {
-//     page: document.querySelector("body"),
-//     form: document.querySelector(".header__form"),
-//     btnSearch: document.querySelector(".form__btn"),
-    // delete: document.querySelector(".js-delete"),
-    // more: document.querySelector(".js-more"),
-    // list: document.querySelector(".picture__list"),
-    // listFavorites: document.querySelector(".picture__list.favorites__list"),
-    // btnFavourite:  document.querySelector(".header__favorites"),
-    // logo: document.querySelector(".header__logo"),
-    // home: document.querySelector(".js-home"),
-    // prev: document.querySelector(".js-prev"),
-    // next: document.querySelector(".js-next"),
-    // select: document.querySelector(".js-select"),
-    // close: document.querySelector(".js-close")
-// };
-
-// function favoriteOpen(event) {
-//     event.preventDefault();
-//     console.log(refs.btnFavourite.classList.contains('js-home'))
-//     if(refs.btnFavourite.classList.contains('js-home')){
-//         refs.btnFavourite.classList.remove('js-home');
-//         refs.page.classList.remove('favorites--active');
-//         refs.btnFavourite.textContent = "Избранное";
-//         return
-//     }
-//     refs.page.classList.add('favorites--active');
-//     refs.btnFavourite.textContent = "На главную";
-//     refs.btnFavourite.classList.add('js-home');
-
-// }
-// function favoriteClose(event) {
-//     event.preventDefault();
-//     refs.page.classList.remove('favorites--active');
-//     refs.btnFavourite.textContent = "Избранное";
-//     refs.btnFavourite.classList.remove('js-home');
-// }
-
-// refs.btnFavourite.addEventListener('click', favoriteOpen);
-// refs.logo.addEventListener('click', favoriteClose);
-
-/* const refs = {
-     page: document.querySelector("body"),
-     form: document.querySelector(".header__form"),
-     btnSearch: document.querySelector(".form__btn"),
- delete: document.querySelector(".js-delete"),
- more: document.querySelector(".js-more"),
- list: document.querySelector(".picture__list"),
- listFavorites: document.querySelector(".picture__list.favorites__list"),
- btnFavourite:  document.querySelector(".header__favorites"),
- logo: document.querySelector(".header__logo"),
- home: document.querySelector(".js-home"),
- prev: document.querySelector(".js-prev"),
- next: document.querySelector(".js-next"),
- select: document.querySelector(".js-select"),
- close: document.querySelector(".js-close")
- };
-
- const popUpClose = () => refs.page.classList.remove('modale-open');
-
- function popUpOpen(event) {
-     event.preventDefault();
-     const target = event.target;
-     console.log("event target: ", target);  //посмотрите что тут
-     if (target.nodeName !== "LI") return;
-     refs.page.classList.add('modale-open');
- }
- function favoriteOpen(event) {
-     event.preventDefault();
-     console.log(refs.btnFavourite.classList.contains('js-home'))
-     if(refs.btnFavourite.classList.contains('js-home')){
-         refs.btnFavourite.classList.remove('js-home');
-         refs.page.classList.remove('favorites--active');
-         refs.btnFavourite.textContent = "Избранное";
-         return
-     }
-     refs.page.classList.add('favorites--active');
-     refs.btnFavourite.textContent = "На главную";
-     refs.btnFavourite.classList.add('js-home');
-
- }
- function favoriteClose(event) {
-     event.preventDefault();
-     refs.page.classList.remove('favorites--active');
-     refs.btnFavourite.textContent = "Избранное";
-     refs.btnFavourite.classList.remove('js-home');
- }
-
- refs.list.addEventListener('click', popUpOpen);
- refs.close.addEventListener('click', popUpClose);
- refs.btnFavourite.addEventListener('click', favoriteOpen);
- refs.logo.addEventListener('click', favoriteClose);
-*/
